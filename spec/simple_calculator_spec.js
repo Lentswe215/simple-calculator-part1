cc3b91aa734154f9e618d893425573555a6b4b78
Lentswe215 <ephraimmamonong@gmail.com>
2019-12-17 23:52:02 +0200

    added .gitignore file

---
+++ spec/simple_calculator_spec.js
@@ -0,0 +1,27 @@
+let myAdd = require("../src/simple_calculator");
+let add = myAdd.add;
+let multiply = myAdd.multiply;
+
+describe("add", () => {
+  it("should return sum of 2 numbers", () => {
+    expect(add(1, 2)).toBe(3);
+  });
+  it("should return sum of 2 numbers", () => {
+    expect(add(-1, -1)).toBe(-2);
+  });
+  it("should return sum of multiple numbers ", () => {
+    expect(add(1, 2, 3, 4)).toBe(10);
+  });
+});
+
+describe("multiply", () => {
+  it("should return result of multiplied numbers", () => {
+    expect(multiply(1, 3)).toBe(3);
+  });
+  it("should return the result of multiplied multiple numbers", () => {
+    expect(multiply(1, 2, 3, 4, 5)).toBe(120);
+  });
+  it("should return result of multiplied numbers", () => {
+    expect(multiply(-1, 3)).toBe(-3);
+  });
+});
